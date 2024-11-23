132201022f2fe0ccfddad214db532a075be135df
lokesh-18vk <lokeshpatil0018@gmail.com>
2024-11-23 15:57:16 +0530

    initial

---
+++ api/index.js
@@ -0,0 +1,26 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+import dbConnect from "../config/database.js";
+import router from "../routes/daw.js";
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.use("/api/v1", router);
+
+dbConnect();
+  
+app.listen(PORT, () => {
+  console.log("Server is running at port:", PORT);
+});
+
+app.get("/",(req,res)=>{
+  res,json({
+    success:true,
+    message:"Server Connected Successfully"
+  })
+})
